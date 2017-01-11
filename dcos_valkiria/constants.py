6361f578bbd331b9784d69f8eee97d96510de0a4
eruizgar <eruiz@stratio.com>
2017-01-11 10:56:21 +0100

    Fix version in constants.

--- dcos_valkiria/constants.py
+++ dcos_valkiria/constants.py
@@ -1,5 +1,5 @@
-version = '0.1.0-RC2-SNAPSHOT'
-valkiria_version = '0.1.0-RC2-SNAPSHOT'
+version = open("VERSION").read().rstrip("\n")
+valkiria_version = '0.1.0'
 """DCOS valkiria version"""
 url_install = 'http://sodio.stratio.com/nexus/content/sites/paas/valkiria/{0}/valkiria-{0}.tar.gz'.format(valkiria_version)
 url_list = 'http://127.0.0.1:9050/api/v1/list'
