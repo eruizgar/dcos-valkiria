181f3c4f7d4da2a79ed6cd3fa42c6b2da5b57cb7
stratiocommit <stratiocommit@stratio.com>
2017-01-11 09:41:40 +0000

    Prepare for next version

--- dcos_valkiria/constants.py
+++ dcos_valkiria/constants.py
@@ -1,5 +1,5 @@
-version = '0.1.0-SNAPSHOT'
-valkiria_version = '0.1.0'
+version = '0.2.0-SNAPSHOT'
+valkiria_version = '0.2.0-SNAPSHOT'
 """DCOS valkiria version"""
 url_install = 'http://sodio.stratio.com/nexus/content/sites/paas/valkiria/{0}/valkiria-{0}.tar.gz'.format(valkiria_version)
 url_list = 'http://127.0.0.1:9050/api/v1/list'
