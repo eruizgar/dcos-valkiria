badc713b1405d071013304881ae3bdec2182be5a
eruizgar <eruiz@stratio.com>
2017-01-11 11:39:42 +0100

    Fix change version

--- bin/change-version.sh
+++ bin/change-version.sh
@@ -8,5 +8,5 @@ PATH=$(pwd)/dist:$PATH
 
 echo "Modifying version to: $1"
 echo $1 > VERSION
-sed -e "s/version = .*/version = '$1'/" dcos_valkiria/constants.py > tmp
+sed -e "s/^version = .*/version = '$1'/" dcos_valkiria/constants.py > tmp
 mv tmp dcos_valkiria/constants.py
